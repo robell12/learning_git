1ff12e9b6199fa6b83dbfb2993478e5130361137
Robert Bell <robell7837@hotmail.com>
2016-10-26 10:25:53 -0600

    name age is old check

---
+++ test.rb
@@ -0,0 +1,13 @@
+puts "Learing Git/ GitHub"
+
+puts "What is your name?"
+name = gets.strip
+
+puts "What is your age?"
+age = gets.strip
+
+if name == 'dave' && age == '35'
+	puts "You're old"
+else
+	puts "Hello, #{name} you are #{age} years old!"
+end
