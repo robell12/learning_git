c752dea2ee990500cf87998532c5e78e477e76d2
Robert Bell <robell7837@hotmail.com>
2016-10-26 11:12:50 -0600

    age case statement

--- test.rb
+++ test.rb
@@ -6,8 +6,20 @@ name = gets.strip
 puts "What is your age?"
 age = gets.strip
 
-if name == 'dave' && age == '35'
+if name == 'dave' && age == '35'	
 	puts "You're old"
 else
 	puts "Hello, #{name} you are #{age} years old!"
+end
+
+# Feature Branch Code
+case age
+  when 18
+  	'You can no smoke cigs'
+  when 21
+  	'Yay, drinking and gambling'
+  when 26
+  	'This the last milestone, lets rent a car'
+  else
+  	'cool, you have an age!'
 end
